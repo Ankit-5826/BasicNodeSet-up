1c67c8eda2b83d7775d502619825f77c529f6481
Ankit-5826 <ankitvishwakarma5826@gmail.com>
2026-01-20 02:03:43 +0100

    basic package-Setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import app from "./app.js";
+import dotenv from "dotenv";
+
+// Env Path setting
+dotenv.config({ path: "./.env" });
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
